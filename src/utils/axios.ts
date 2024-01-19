4e83597b3755bd694a57dce224d67f3e96e12c32
ferlarag <89590031+ferlarag@users.noreply.github.com>
2024-01-19 09:09:34 -0600

    Backend URL Change

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBackend = axios.create({
-  baseURL: "https://www.wallet-wise.cloud/api",
+  baseURL: "https://eb-backend-example-dev.us-west-2.elasticbeanstalk.com/api",
 });
 
 export default axiosBackend;
